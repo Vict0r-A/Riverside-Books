a491513a76c3271310e75c557ba35f31f57e56c3
Victor Adams <thevictoradams@gmail.com>
2026-06-16 23:14:35 +0100

    update of the path in the fileLocation variable, so that it only runs within the src directory

--- src/loadFAQ.ts
+++ src/loadFAQ.ts
@@ -1,15 +1,16 @@
 //All FAQs must be converted into embeddings, openai api needs and array to do this
-//this function will convert the json into an array
+//this function will convert the json into an array of questions
 
 
 import faqs from "./faqs.json";
 import fs from "node:fs";
 import path from "node:path";
-import { FAQs } from "./types";
-export function loadFAQs() : FAQs[] {
-    const fileLocation = path.join(process.cwd(), "src", "faqs.json");
+import { FAQ } from "./types";
+export function loadFAQs() : FAQ[] {
+    const fileLocation = path.join(process.cwd(), "faqs.json");
     const fileContents = fs.readFileSync(fileLocation, "utf-8");
-    const faqsArray = JSON.parse(fileContents) as FAQs[];
+    const faqsArray = JSON.parse(fileContents) as FAQ[];
+
 
     return faqsArray;
 
